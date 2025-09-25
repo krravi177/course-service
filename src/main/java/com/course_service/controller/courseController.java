520e9a550f6419536f5bf7c662987833a041856b
Ravindra Kumar <krravi177@gmail.com>
2025-09-25 23:29:00 +0530

    uncomment all the controller

--- src/main/java/com/course_service/controller/courseController.java
+++ src/main/java/com/course_service/controller/courseController.java
@@ -58,16 +58,16 @@ public class courseController {
         }
     }
 
-//    @GetMapping("/health")
-//    public String healthCheck() {
-//        return "UP";
-//    }
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "UP";
+    }
 
 
-//    @GetMapping("/welcome")
-//    public String greetings() {
-//        return "Hello Techie , AWS CICD Example working fine !";
-//    }
+    @GetMapping("/welcome")
+    public String greetings() {
+        return "Hello Techie , AWS CICD Example working fine !";
+    }
 
 
 }
